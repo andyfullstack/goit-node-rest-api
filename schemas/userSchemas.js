87bf5e9aabb7bec106d1302f44023b315f41008b
andyfullstack <andriikoliadenko@gmail.com>
2024-03-06 22:23:33 +0200

    upd with subs

--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -11,6 +11,10 @@ const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+const subscriptionSchema = Joi.object({
+  subscription: Joi.string().valid("starter", "pro", "business"),
+});
+
 export const schemas = {
   registerSchema,
   loginSchema,
