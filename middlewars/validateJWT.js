d630e932250e1f0d5916852140ed1099b62510e9
andyfullstack <andriikoliadenko@gmail.com>
2024-03-07 13:43:31 +0200

    update imp dotnv

--- middlewars/validateJWT.js
+++ middlewars/validateJWT.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import "dotenv/config";
 import { User } from "../models/user.js";
 import { HttpError } from "../helpers/index.js";
 
@@ -7,12 +8,15 @@ const { SECRET_KEY } = process.env;
 const validateJWT = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
+  console.log(token);
   if (bearer !== "Bearer") {
     next(HttpError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
+    console.log(id);
     const user = await User.findById(id);
+    console.log(user);
     if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
     }
