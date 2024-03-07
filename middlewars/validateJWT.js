082add4a74d1662616cf18e0618373247bfdef45
andyfullstack <andriikoliadenko@gmail.com>
2024-03-07 13:44:50 +0200

    upd

--- middlewars/validateJWT.js
+++ middlewars/validateJWT.js
@@ -8,15 +8,13 @@ const { SECRET_KEY } = process.env;
 const validateJWT = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  console.log(token);
+
   if (bearer !== "Bearer") {
     next(HttpError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
-    console.log(id);
     const user = await User.findById(id);
-    console.log(user);
     if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
     }
